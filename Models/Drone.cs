fe3fb04014c7c923175adeaad1846c384b47444a
Pedro Garcia <pedro8321go@gmail.com>
2024-07-26 16:12:29 -0400

    adding drone and medicine models

---
+++ Models/Drone.cs
@@ -0,0 +1,13 @@
+﻿namespace DronesTechAPI.Models;
+
+public class Drone
+{
+    //TODO: is necessary validate the fields
+    
+    public string SerialNumber { get; set; }
+    public string DeviceModel { get; set; }
+    public int LimitWeight { get; set; }
+    public int BatteryCapacity { get; set; }
+    public string State { get; set; }
+    public List<Medicine> Medicines { get; set; } = new List<Medicine>();
+}
---
+++ Models/Medicine.cs
@@ -0,0 +1,11 @@
+﻿namespace DronesTechAPI.Models;
+
+public class Medicine
+{
+    //TODO: is necessary validate the fields
+    
+    public string Name { get; set; }
+    public int Weight { get; set; }  // this value is in grams
+    public string Code { get; set; }
+    public string Image { get; set; }
+}
