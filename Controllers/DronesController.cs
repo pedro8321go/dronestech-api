7be262488d43da260a7ee28d4c5a923c21af7cdf
Pedro Garcia <pedro8321go@gmail.com>
2024-07-26 17:05:06 -0400

    adding initial structure of drone controller

---
+++ Controllers/DronesController.cs
@@ -0,0 +1,45 @@
+﻿using DronesTechAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DronesTechAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DronesController: ControllerBase
+{
+    // register new drone
+    [HttpPost]
+    public IActionResult RegisterDrone(Drone drone)
+    {
+        // link to the DroneService
+    }
+    
+    // Load drone with medicine
+    [HttpPost("{serialNumber}/load")]
+    public IActionResult LoadDrone(string serialNumber, List<Medicine> medicines)
+    {
+        // link to the DroneService
+    }
+    
+    // Get available drones
+    [HttpGet("available")]
+    public IActionResult GetAvailableDrones()
+    {
+        
+    }
+    
+    // Get battery level of a drone
+    [HttpGet("{serialNumber}/battery")]
+    public IActionResult GetBatteryLevel(string serialNumber)
+    {
+        
+    }
+    
+    // Check load medicine weight 
+    [HttpGet("{serialNumber}/weight")]
+    public IActionResult CheckMedicinesWeight(string serialNumber)
+    {
+        
+    }
+    
+}
