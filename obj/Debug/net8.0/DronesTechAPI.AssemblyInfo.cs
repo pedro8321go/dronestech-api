38da319517130279fcfa1c6373fb34346094c660
Pedro Garcia <pedro8321go@gmail.com>
2024-07-26 20:14:30 -0400

    regular save

--- obj/Debug/net8.0/DronesTechAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DronesTechAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DronesTechAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be262488d43da260a7ee28d4c5a923c21af7cdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("DronesTechAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DronesTechAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
