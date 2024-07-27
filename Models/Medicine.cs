8675eb14d0b3bcbf81b0d5c4ed2c907931183cf1
Pedro Garcia <pedro8321go@gmail.com>
2024-07-26 20:16:20 -0400

    removing dot in error message of name validation

--- Models/Medicine.cs
+++ Models/Medicine.cs
@@ -4,7 +4,7 @@ namespace DronesTechAPI.Models;
 public class Medicine
 {
     [Required(ErrorMessage = "Name is required")]
-    [RegularExpression("^[a-zA-Z0-9-_]+$", ErrorMessage = "The name can only contain letters, numbers, hyphens and underscores.")]
+    [RegularExpression("^[a-zA-Z0-9-_]+$", ErrorMessage = "The name can only contain letters, numbers, hyphens and underscores")]
     public string? Name { get; set; }
     [Required(ErrorMessage = "Weight value is required.")]
     [Range(1, 500, ErrorMessage = "Limit weight value must be in range 1 to 500 grams")]
